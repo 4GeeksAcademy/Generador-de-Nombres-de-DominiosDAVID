1be450adce37081ec58872b6c88acec6504d954d
DVBDJ <163288149+DVBDJ@users.noreply.github.com>
2024-05-22 11:13:42 +0000

    main

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generadorDominio = () => {
+  let pronombres = ["el", "nuestro", "este"];
+  let adjetivos = ["increible", "absurdo", "cruel"];
+  let sustantivos = ["buscador", "blog", "navegador", "almacen"];
+  let extensiones = [".com", ".es", ".net"];
+
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let sustantivo of sustantivos) {
+        for (let extension of extensiones) {
+          console.log("www." + pronombre + adjetivo + sustantivo + extension);
+        }
+      }
+    }
+  }
 };
+
+generadorDominio();
